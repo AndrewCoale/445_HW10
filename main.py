955aa886514f4336feae149e89f9e7047f862de3
AndrewCoale <55396067+AndrewCoale@users.noreply.github.com>
2025-03-26 23:38:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report
 
 # Load the dataset
-file_path = "partial_credit.csv"  # Update the path if needed
+file_path = "partial_credit.csv"
 df = pd.read_csv(file_path)
 
 # Drop the irrelevant column
